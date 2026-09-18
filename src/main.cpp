8df75e9033d98b86ef7a8cce4187d7bbe6fd82e4
ramondeleonca <ramondeleonca@gmail.com>
2026-09-18 08:30:42 -0600

    Initial commit with data and setup files

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int choice;
+
+    cout << "File to search" << endl;
+    cout << "(1) log607-1.txt" << endl;
+    cout << "(2) log607-2.txt" << endl;
+    cout << "Selection: ";
+    cin >> choice;
+
+    
+    
+
+    return 0;
+}
